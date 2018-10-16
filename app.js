e91e83186bd717c8028c24ed9a0a790e19ae4fe3
Bruno Scherer <bruno.raphael96@gmail.com>
2018-10-16 10:06:21 -0300

    add: cpf validation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   VMasker(document.getElementById("telefone")).maskPattern("(99) 999-999-999");
   VMasker(document.getElementById("dataNascimento")).maskPattern("99/99/9999");
+  VMasker(document.getElementById("cpf")).maskPattern("999.999.999-99");
 })
 
 new Vue({
@@ -37,12 +38,12 @@ new Vue({
       } else if (!/^\D+$/.test(this.nome)) {
         this.errors.push('Não são permitidos números no nome.');
       } else if (!/^[A-Za-zÀ-ú]+$/.test(this.nome)) {
-        this.errors.push('Não são permitidos caractéres especiais no nome.');
+        this.errors.push('Não são permitidos caracteres especiais no nome.');
       }
 
       if (!this.telefone) {
         this.errors.push('O telefone é obrigatório.');
-      } else if (this.telefone.length != 11) {
+      } else if (this.telefone.length != 17) {
         this.errors.push('Número de telefone inválido.');
       }
 
@@ -52,37 +53,41 @@ new Vue({
         this.errors.push('O email é inválido.');
       }
 
-       if (!this.dataNascimento) {
-         this.errors.push('A data de nascimento é obrigatória.');
-       } else if (this.dataNascimento.length < 8) {
+      if (!this.dataNascimento) {
+        this.errors.push('A data de nascimento é obrigatória.');
+      } else if (this.dataNascimento.length < 8) {
         this.errors.push('Data inválida');
-       } else {
-         this.validBirthday(this.dataNascimento);
-       }
+      } else {
+        this.validBirthday(this.dataNascimento);
+      }
 
-       if (!this.cpf) {
-         this.errors.push('O cpf é obrigatório.');
-       }
+      if (!this.cpf) {
+        this.errors.push('O cpf é obrigatório.');
+      } else if (this.cpf.length < 14) {
+        this.errors.push('O cpf está incompleto.');
+      } else if (!this.validCpf(this.cpf)) {
+        this.errors.push('CPF inválido.');
+      }
 
-       if (!this.cidade) {
-         this.errors.push('A cidade é obrigatória.');
-       }
+      if (!this.cidade) {
+        this.errors.push('A cidade é obrigatória.');
+      }
 
-       if (!this.estado) {
-         this.errors.push('O estado é obrigatório.');
-       }
+      if (!this.estado) {
+        this.errors.push('O estado é obrigatório.');
+      }
 
-       if (!this.linguagens) {
-         this.errors.push('As linguagens são obrigatórias.');
-       }
+      if (!this.linguagens) {
+        this.errors.push('As linguagens são obrigatórias.');
+      }
 
-       if (!this.idiomas) {
-         this.errors.push('Os idiomas são obrigatórios.');
-       }
+      if (!this.idiomas) {
+        this.errors.push('Os idiomas são obrigatórios.');
+      }
 
-       if (!this.descricao) {
-         this.errors.push('A descricao é obrigatória.');
-       }
+      if (!this.descricao) {
+        this.errors.push('A descricao é obrigatória.');
+      }
 
       e.preventDefault();
 
@@ -99,10 +104,48 @@ new Vue({
         this.errors.push('Dia inválido');
       } else if (arrDate[2] > today.getFullYear) {
         this.errors.push('Ano inválido');
-      } else if ((Math.floor((today - bDay) / (1000 * 60 * 60 * 24 * 365))) < 18 ) {
+      } else if ((Math.floor((today - bDay) / (1000 * 60 * 60 * 24 * 365))) < 18) {
         this.errors.push('Menor de 18 anos');
       }
 
+    },
+    validCpf: function (cpf) {
+      cpf = cpf.replace(/[^\d]+/g, '');
+      if (cpf == "00000000000" ||
+        cpf == "11111111111" ||
+        cpf == "22222222222" ||
+        cpf == "33333333333" ||
+        cpf == "44444444444" ||
+        cpf == "55555555555" ||
+        cpf == "66666666666" ||
+        cpf == "77777777777" ||
+        cpf == "88888888888" ||
+        cpf == "99999999999") {
+        return false;
+      }
+      let add = 0;
+      for (i = 0; i < 9; i++) {
+        add += parseInt(cpf.charAt(i)) * (10 - i);
+      }
+      let rev = 11 - (add % 11);
+      if (rev == 10 || rev == 11) {
+        rev = 0;
+      }
+      if (rev != parseInt(cpf.charAt(9))) {
+        return false;
+      }
+      add = 0;
+      for (i = 0; i < 10; i++) {
+        add += parseInt(cpf.charAt(i)) * (11 - i);
+      }
+      rev = 11 - (add % 11);
+      if (rev == 10 || rev == 11) {
+        rev = 0;
+      }
+      if (rev != parseInt(cpf.charAt(10))) {
+        return false;
+      }
+      return true;
     },
     scrollTop: function () {
       document.body.scrollTop = 0;
